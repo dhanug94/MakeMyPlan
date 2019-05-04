0833d6ed52397b777dbf8d69fb176916b4f9fedd
unknown <dhanashri.galgalikar11@gmail.com>
2019-05-04 09:40:32 +0530

    second commit

---
+++ MakeMyPlan/src/main/java/com/example/MakeMyPlan/MakeMyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.MakeMyPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyPlanApplication.class, args);
+	}
+
+}
